57ae0c7579cc5af4a035569362fa7ed572787239
Patrick Zhang <patrick.zhang@mongodb.com>
2023-03-29 09:47:12 -0400

    more linked list practice questions

--- src/main/java/linkedListPractice/Practice.java
+++ src/main/java/linkedListPractice/Practice.java
@@ -1,5 +1,8 @@
 package linkedListPractice;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Practice {
 	static class ListNode {
 		int val;
@@ -64,6 +67,34 @@ public class Practice {
 		return head;
 	}
 	
+	public static ListNode deleteNodeOther(ListNode head, int val) {
+		if (head == null) {
+			return head;
+		}
+		
+		// Special case of deleting the head
+		if (head.val == val) {
+			return head.next;
+		}
+		
+		// Otherwise, delete node that isn't the first node in the list
+		ListNode curr = head;
+		ListNode prev = null;
+		
+		while (curr != null) {
+			if (curr.val == val) {
+				// Delete the node and return the original head!
+				prev.next = curr.next;
+				return head;
+			}
+			prev = curr;
+			curr = curr.next;
+		}
+		
+		// No element was deleted
+		return head;
+	}
+	
 	public static ListNode reverseList(ListNode head) {
 		// input validation
 		if (head == null) {
@@ -85,19 +116,162 @@ public class Practice {
 		return prev;
 	}
 	
+	public static ListNode removeDuplicatesWithSpace(ListNode head) {
+		Set<Integer> seen = new HashSet<Integer>();
+		
+		ListNode curr = head;
+		ListNode prev = null;
+		while (curr != null) {
+			// Found a duplicate, remove it from the list!
+			if (seen.contains(curr.val)) {
+				prev.next = curr.next;
+			} else {
+				// Otherwise, first time encountering an element, add this to the seen set
+				seen.add(curr.val);
+				// Note how we only set a new prev if we don't remove the current element
+				prev = curr;
+			}
+			curr = curr.next;
+		}
+		
+		// Head should always be the same- never will change the head as the result of this operation
+		return head;
+	}
 	
+	public static ListNode removeDuplicatesWithoutSpace(ListNode head) {
+		ListNode curr = head;
+		
+		// Each time, curr will definitely be pointing to a unique element
+		while (curr != null) {
+			// For each node, remove all duplicates in the list
+			ListNode runner = curr;
+			
+			while (runner.next != null) {
+				// Found a duplicate later in the list, remove this!
+				if (runner.next.val == curr.val) {
+					runner.next = runner.next.next;
+				} else {
+					runner = runner.next;
+				}
+			}
+			
+			// Move onto the next unique element
+			curr = curr.next;
+		}
+		
+		return head;
+	}
+	
+	public static ListNode removeDuplicatesWithoutSpacePrevPointer(ListNode head) {
+		if (head == null) {
+			return head;
+		}
+		
+		// Curr will point to each unique element in the list and remove all duplicates
+		ListNode curr = head;
+		while (curr != null) {
+			ListNode runner = curr.next;
+			ListNode prev = curr;
+			
+			while (runner != null) {
+				if (runner.val == curr.val) {
+					prev.next = runner.next;
+				} else {
+					prev = runner;
+				}
+				// Always moving the runner, but only updating prev if not a duplicate,
+				// otherwise prev refers to the same value
+				runner = runner.next;
+			}
+			
+			curr = curr.next;
+		}
+		
+		// Never will modify the head of the list
+		return head;
+	}
+	
+	// If k == 1, this is referring to the last element
+	public static ListNode returnKthToLastElementBruteForce(ListNode head, int k) {
+		// Obtain the size, and then iterate again to return the answer
+		int size = size(head);
+		
+		// Invalid k passed in
+		if (k <= 0 || k > size) {
+			return null;
+		}
+		
+		ListNode answer = head;
+		for (int i = 0; i < size - k; i++) {
+			answer = answer.next;
+		}
+		
+		return answer;
+	}
+	
+	public static int size(ListNode head) {
+		int size = 0;
+		ListNode curr = head;
+		
+		while (curr != null) {
+			size++;
+			curr = curr.next;
+		}
+		
+		return size;
+	}
+	
+	public static ListNode returnKthToLastOptimized(ListNode head, int k) {
+		// Create two pointers, set them k length apart, and then the slow pointer should be
+		// pointing to the answer
+		if (head == null) {
+			return null;
+		}
+		
+		// Invalid input k
+		if (k <= 0) {
+			throw new IllegalArgumentException("k provided was too small");
+		}
+		
+		ListNode slow = head;
+		ListNode fast = head;
+		
+		// Offset slow and fast pointer by k elements
+		for (int i = 0; i < k; i++) {
+			if (fast != null) {
+				fast = fast.next;
+			} else {
+				// Value of k provided was too large!
+				throw new IllegalArgumentException("k provided was too large");
+			}
+		}
+		
+		// Now, keep iterating until fast is null and slow will point to the answer
+		while (fast != null) {
+			slow = slow.next;
+			fast = fast.next;
+		}
+		
+		return slow;
+	}
 
 	public static void main(String[] args) {
 		ListNode head = add(null, 10);
-		
+		head = add(head, 10);
 		head = add(head, 15);
 		head = add(head, 30);
+		head = add(head, 30);
+		head = add(head, 30);
+		head = add(head, 30);
+		head = add(head, 15);
+		head = add(head, 15);
 		
-		head = deleteNode(head, 300);
-		
-		head = reverseList(head);
-		
+		head = removeDuplicatesWithoutSpacePrevPointer(head);
+		System.out.println("Size is: " + size(head));
 		printList(head);
+		
+		ListNode tmp = returnKthToLastOptimized(head, 2);
+		System.out.println(tmp.val);
 	}
 
 }
